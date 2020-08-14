8abb61bfe91d5d6c3dacec5f3c45da99b6bc9fe5
Derek Borland <952219+derekborland@users.noreply.github.com>
2020-08-14 11:06:28 -0700

    update npm packages to latest versions (#3)
    * update npm packages to latest versions
    
    * added test document

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,4 +10,23 @@
 
   console.log(materials.map(material => material.length));
   // expected output: Array [8, 6, 7, 9]
+
+  // es6 class
+  class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+
+    get area() {
+      return this.calcArea();
+    }
+
+    calcArea() {
+      return this.height * this.width;
+    }
+  }
+  const square = new Rectangle(10, 10);
+  console.log(square.area); // 100
+
 })();
